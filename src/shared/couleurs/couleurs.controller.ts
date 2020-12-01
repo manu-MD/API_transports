063fb29d390830d14755d556abad1103ac4b5929
KAM <k.ziane@mesdocteurs.com>
2020-12-01 16:24:56 +0100

    Add couleurs controller + service

--- src/shared/couleurs/couleurs.controller.ts
+++ src/shared/couleurs/couleurs.controller.ts
@@ -1,19 +1,38 @@
-import { Controller, Get } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Delete, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Couleurs } from "./couleurs.entity";
 import { CouleursService } from "./couleurs.service";
+import { JwtAuthGuard } from '../../auth/jwt-auth.guards';
 
 @ApiTags('couleurs')
 @Controller('couleurs')
 export class CouleurController {
 
     constructor(
-        private cs: CouleursService
+        private cs: CouleursService,
         ) {
     }
 
-    @Get()
-    findAll(): Promise<Couleurs[]> {
+    // Requête Get findAll
+    @Get() findAll(): Promise<Couleurs[]> {
         return this.cs.findAll();
+    }
+
+    // Requête Get findById
+    @Get(':id')
+    findById(@Param('id') id: string,): Promise<Couleurs> {
+        return this.cs.findById(id);
+    }
+
+    // Requête Post
+    @Post()
+    create(@Body() couleur: Couleurs): Promise<Couleurs> {
+        return this.cs.create(couleur);
+    }
+
+    // Requête Delete
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.cs.delete(id);
     }
 }
--- src/shared/couleurs/couleurs.service.ts
+++ src/shared/couleurs/couleurs.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Connection, DeleteResult, Repository } from 'typeorm';
 import { Couleurs } from './couleurs.entity';
 
 @Injectable()
@@ -8,18 +8,26 @@ export class CouleursService {
     constructor(
         @InjectRepository(Couleurs)
         private readonly couleursRepository: Repository<Couleurs>,
+        private connection: Connection
     ){}
 
-    create(Couleurs: Couleurs): Promise<Couleurs> {
-        return this.couleursRepository.save(Couleurs);
+    // Méthode Get findAll (rechercher toute les couleurs)
+    async  findAll(): Promise<Couleurs[]> {
+        return await this.couleursRepository.find();
     }
 
-    findAll(): Promise<Couleurs[]> {
-        return this.couleursRepository.find({
-            order: {
-                couleur: 'ASC'
-            }
-        });
+    // Méthode Get findById (rechercher une couleur par ID)
+    async findById(id): Promise<Couleurs> {
+        return await this.connection.getRepository(Couleurs).findOneOrFail(id);
     }
 
+    // Méthode Post (création de couleur)
+    async create(couleur: Couleurs): Promise<Couleurs> {
+        return this.couleursRepository.save(couleur);
+    }
+
+    // Méthode Delete (supprimer ID de la couleur)
+    async delete(id): Promise<DeleteResult> {
+        return await this.couleursRepository.delete(id);
+    }
 }
